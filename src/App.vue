b3977716b781e5080697dcdedd1e97c47e65ba5d
Anastasiia Chumak <chumaana@fit.cvut.cz>
2024-02-29 16:36:28 +0100

    Added some html and css

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" placeholder="Search..." />
+    </header>
+    <div class="options-container"></div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header {
+  margin: 30px 0 10px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgb(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+</style>
